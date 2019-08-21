aa4802168e73d369379a90c865c1ae9c5c729469
Marcus Mouyat <marcus@mouyat.com>
2019-08-21 16:05:07 +1000

    Added files to repository

---
+++ Assets/Scripts/ShootBullets.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Mirror;
+
+public class ShootBullets : NetworkBehaviour
+{
+
+    [SerializeField]
+    private GameObject bulletPrefab;
+
+    [SerializeField]
+    private float bulletSpeed;
+
+    void Update()
+    {
+        if (this.isLocalPlayer && Input.GetKeyDown(KeyCode.Space))
+        {
+            this.CmdShoot();
+        }
+    }
+
+    [ClientRpc]
+    void RpcClientShot(Vector3 position)
+    {
+        GameObject bullet = Instantiate(bulletPrefab, position, Quaternion.identity);
+        Rigidbody2D rigid = bullet.GetComponent<Rigidbody2D>();
+        rigid.velocity = Vector2.up * bulletSpeed;
+        Destroy(bullet, 1.0f);
+    }
+
+    [Command]
+    void CmdShoot()
+    {
+        GameObject bullet = Instantiate(bulletPrefab, this.transform.position, Quaternion.identity);
+        bullet.GetComponent<Rigidbody2D>().velocity = Vector2.up * bulletSpeed;
+        NetworkServer.Spawn(bullet);
+        Destroy(bullet, 1.0f);
+    }
+}
